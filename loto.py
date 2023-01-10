88a53be78f782fc23b154e2a6a8ca5bb5943c7ea
MaziniiX <bluenderbot@gmail.com>
2023-01-10 11:56:52 +0100

    yo

--- loto.py
+++ loto.py
@@ -1,11 +1,24 @@
 import random
-seed=int(input("Veuillez rentrez une graine : "))
-tirage=0
-random.seed(seed)
-for i in range(5):
+
+graine=int(input("Veuillez rentrez une graine : ")) #Définition de la graine aléatoire
+random.seed(graine)
+print()
+
+def tirage(): #Fonction de tirages succesifs
+    nbr=int(input("Combien de tirage voulez-vous faire ? (0 pour arrêtez) "))
+    print()
+    if nbr >= 1:
+        for t in range(nbr):
+            for n in range(5):
+                a = round(random.uniform(1, 45))
+                print(a, end=" ")
+            print("\n")
+        tirage()
+
+print("Les boules gagnantes sont :",end=" ")
+for i in range(5): #Tirage des 5 boules gagnantes
     a=round(random.uniform(1, 45))
     print(a,end=" ")
-tirage=int(input("Combien de tirage voulez-vous faire ? (0 pour arrêtez)"))
-for n in range(tirage):
-    a = round(random.uniform(1, 45))
-    print(a, end=" ")
+
+print("\n")
+tirage()
