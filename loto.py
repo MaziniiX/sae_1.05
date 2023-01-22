080f439f92936fc19ed3bac8082fc62f4a74191c
MaziniiX <bluenderbot@gmail.com>
2023-01-22 01:03:47 +0100

    dicho fini + better tri display

--- loto.py
+++ loto.py
@@ -1,64 +1,58 @@
 import random
+import os
 import matplotlib.pyplot as plt
 
-graine = int(input("Veuillez rentrez une graine : "))  # Définition de la graine aléatoire
-random.seed(graine)
+graine = int(input("Veuillez rentrez une graine : "))
+random.seed(graine) # Définition de la graine aléatoire pour la génération des séquences
 print()
 
 print("Les boules gagnantes sont :", end=" ")
-# Tirage des 5 boules gagnantes
-win = random.sample(range(1,45), 5)
-print(win,"\n")
-
+# Tirage des 5 boules gagnantes/ La séquence à chercher dans la série de tirage
+win = random.sample(range(1, 45), 5)
+print(win, "\n")
 
 
 def tirage():
     nbr = int(input("Combien de tirage voulez-vous faire ? (0 pour arrêtez) "))
     print()
     if nbr >= 1:
-        resultat = [] # Initialisation de la liste qui stockera les résultats de tirage
+        resultat = []  # Initialisation de la liste qui stockera les résultats de tirage
         for p in range(nbr):
-            t = random.sample(range(1,45), 5)
+            t = random.sample(range(1, 45), 5)
             t.sort()  # tri des nombres tirés dans l'ordre croissant
-            resultat.append(t) # Ajout des résultats de tirage à la liste
+            resultat.append(t)  # Ajout des résultats de tirage à la liste
         return resultat
 
-def choix_tri():
-    print("Choisissez une méthode de tri:")
-    print("1 - Tri par insertion")
-    print("2 - Tri cocktail")
-    print("3 - Tri fusion")
-    choix = int(input())
-    if choix == 1:
-        tri_insertion(resultat)
-    elif choix == 2:
-        tri_cocktail(resultat)
-    elif choix == 3:
-        tri_fusion(resultat)
-    else:
-        print("Choix non valide.")
-    return resultat
-
-########################################################################################
+"""#####################################################################################
 ####################### Fonctions de tri des séquences de tirages ######################
-########################################################################################
+#####################################################################################"""
 
 def tri_insertion(resultat):
     for i in range(1, len(resultat)):
         cle = resultat[i]
-        j = i-1
+        j = i - 1
         while j >= 0 and resultat[j] > cle:
             resultat[j + 1] = resultat[j]
             j -= 1
         resultat[j + 1] = cle
+
+    for i in range(len(resultat)):
+        print(f"{i+1}.",resultat[i])
+
     return resultat
+
 
 def tri_cocktail(resultat):
     for i in range(len(resultat)):
         for j in range(i, len(resultat)):
             if resultat[i] > resultat[j]:
                 resultat[i], resultat[j] = resultat[j], resultat[i]
+
+    for i in range(len(resultat)):
+        print(f"{i+1}.", resultat[i])
+
     return resultat
+
 
 def tri_fusion(resultat):
     if len(resultat) > 1:
@@ -92,11 +86,77 @@ def tri_fusion(resultat):
 
     return resultat
 
-########################################################################################
-########################################################################################
-########################################################################################
+
+"""#####################################################################################
+############################### Fonctions post-triage ##################################
+#####################################################################################"""
+
+def choix_tri():
+    print("Choisissez une méthode de tri:")
+    print("1 - Tri par insertion")
+    print("2 - Tri cocktail")
+    print("3 - Tri fusion")
+    choix = int(input())
+    if choix == 1:
+        tri_insertion(resultat)
+    elif choix == 2:
+        tri_cocktail(resultat)
+    elif choix == 3:
+        tri_fusion(resultat)
+        for i, seq in enumerate(resultat):
+            print(i + 1, ".", seq)
+    else:
+        print("Choix non valide.")
+    return resultat
+
+def dichotomie_iterative(resultat, win):
+    debut = 0
+    fin = len(resultat) - 1
+    while debut <= fin:
+        milieu = (debut + fin) // 2
+        if resultat[milieu] == win:
+            return milieu
+        elif resultat[milieu] < win:
+            debut = milieu + 1
+        else:
+            fin = milieu - 1
+    return None
+
+def dichotomie_recursive(resultat, win, debut, fin):
+    if debut > fin:
+        return None
+    milieu = (debut + fin) // 2
+    if resultat[milieu] == win:
+        return milieu
+    elif resultat[milieu] < win:
+        return dichotomie_recursive(resultat, win, milieu + 1, fin)
+    else:
+        return dichotomie_recursive(resultat, win, debut, milieu - 1)
+
+def choix_dichotomie(resultat, win):
+    print("Choisissez une méthode de recherche dichotomique:")
+    print("1 - Recherche dichotomique itérative")
+    print("2 - Recherche dichotomique récursive")
+    choix = int(input())
+    if choix == 1:
+        correspondance = dichotomie_iterative(resultat, win)
+    elif choix == 2:
+        correspondance = dichotomie_recursive(resultat, win, 0, len(resultat) - 1)
+    else:
+        print("Choix non valide.")
+        return None
+    if correspondance is not None:
+        print(f"La séquence gagnante a été trouvée à la position {correspondance+1} dans la liste des résultats.")
+    else:
+        print("La séquence gagnante n'a pas été trouvée dans la liste des résultats.")
+    return correspondance
+
+"""#######################################################################################
+##########################################################################################
+#######################################################################################"""
 
 resultat = tirage()
+#resultat.append(win) #Permet de tester la fonction de recherche dichotomique indépendamment des séquences générées par la fonction de tirage
 resultat = choix_tri()
-print(resultat)
-
+print()
+correspondance = choix_dichotomie(resultat, win)
